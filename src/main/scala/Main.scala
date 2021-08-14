ca41e06f4ffac54668eb4213c82497604643501e
kazuhi-ra <kazuhira4126@gmail.com>
2021-08-15 01:34:04 +0900

    いろいろ出力してみる

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,11 @@
 object Main extends App {
   println("Hello, World!")
+
+  println(0xff)
+  println(1e308)
+  println(9223372036854775807L)
+  println(922337203685477580.7)
+  println(1.00000000000000000001 == 1)
+  println("\u3042")
+  println("\ud842\udf9f")
 }

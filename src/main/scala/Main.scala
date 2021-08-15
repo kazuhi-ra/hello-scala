09694c5f2a9069b4abe5f8337d7919b76acaec2f
kazuhi-ra <kazuhira4126@gmail.com>
2021-08-15 22:16:06 +0900

    Listとパターンマッチを使ってみる

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,10 @@
 object Main extends App {
-  println(User.printUser(new User("yo", 25)))
+  val lst = List(List("A"), List("B", "C"))
+
+  lst match {
+    case List(List("A"), List(x, y)) if x != "C" =>
+      println(x)
+    case _ =>
+      println("nothing")
+  }
 }

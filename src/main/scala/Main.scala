e039dbdcb3f01fcfbaa811ab1e86d41c31fe2635
kazuhi-ra <kazuhira4126@gmail.com>
2021-08-15 22:38:38 +0900

    中間パターンを使ってみる

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,10 @@
 object Main extends App {
-  val x = "A"
+  val lst = List("A", "B", "C", "D", "E")
 
-  x match {
-    case "a" | "A" =>
-      println(x)
+  lst match {
+    case "A" :: b :: c :: _ =>
+      println(b)
+      println(c)
     case _ =>
       println("nothing")
   }

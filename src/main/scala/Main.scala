12d73f37f71f6c35631a20754d9189c23ccf065a
kazuhi-ra <kazuhira4126@gmail.com>
2021-08-21 12:04:05 +0900

    fizzbuzz

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,12 @@
 import java.util.Locale
 
 object Main extends App {
-  val obj: AnyRef = "String Literal"
-
-  obj match {
-    case v: java.lang.Integer =>
-      println("Integer!")
-    case v: String =>
-      println(v.toUpperCase(Locale.ENGLISH))
+  def fizzbuzz(i: Int) = (i % 3 == 0, i % 5 == 0) match {
+    case (true, true)   => "fizzbuzz"
+    case (true, false)  => "fizz"
+    case (false, true)  => "buzz"
+    case (false, false) => i
   }
+
+  println((1 to 30).map(i => fizzbuzz(i)))
 }
